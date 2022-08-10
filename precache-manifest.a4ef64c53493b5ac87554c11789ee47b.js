64144bd33f8c03ce5bdd285915b65b3cb8948f8d
Sanket Chaudhari <78204805+sanketchaudhari3009@users.noreply.github.com>
2022-08-10 21:25:38 +0530

    Updates

---
+++ precache-manifest.a4ef64c53493b5ac87554c11789ee47b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79007786407cd4ba59bf",
+    "url": "/flashcard-quiz/static/css/main.b9979793.chunk.css"
+  },
+  {
+    "revision": "79007786407cd4ba59bf",
+    "url": "/flashcard-quiz/static/js/main.49c23739.chunk.js"
+  },
+  {
+    "revision": "b5ec9f56d7867ab93ffe",
+    "url": "/flashcard-quiz/static/js/runtime~main.9ae54858.js"
+  },
+  {
+    "revision": "a1216dbaf4125d931517",
+    "url": "/flashcard-quiz/static/js/2.4cc82278.chunk.js"
+  },
+  {
+    "revision": "27bf0e3f9451c9b80537382903fb4427",
+    "url": "/flashcard-quiz/index.html"
+  }
+];
